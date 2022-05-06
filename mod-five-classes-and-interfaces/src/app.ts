ce08f6a66dbe8212ce4439c1b2ad31d19c25883d
Tim Crisp <timothycrisp@gmail.com>
2022-05-06 13:03:49 -0500

    complete module 5

--- mod-five-classes-and-interfaces/src/app.ts
+++ mod-five-classes-and-interfaces/src/app.ts
@@ -1,26 +1,39 @@
-// interface - describes the structure of an object
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
 
-interface Greetable {
-  name: string;
-  age: number;
+let add: AddFn;
 
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
   name: string;
+  age = 30;
 
   constructor(n: string) {
-    this.name = n; 
+    this.name = n;
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name)
+    console.log(phrase + ' ' + this.name);
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Tim')
+user1 = new Person('Max');
+// user1.name = 'Manu';
 
-user1.greet('Hi there')
+user1.greet('Hi there - I am');
+console.log(user1);
